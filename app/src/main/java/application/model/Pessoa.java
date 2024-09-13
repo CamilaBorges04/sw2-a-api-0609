14326ce1a17bb849c1f46609448c70b84fe4486b
CamilaBorges04 <camilaescola998@gmail.com>
2024-09-13 11:28:04 +0000

    save

--- app/src/main/java/application/model/Pessoa.java
+++ app/src/main/java/application/model/Pessoa.java
@@ -1,8 +1,19 @@
 package application.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String nome;
     private int idade;
 
